515164dfcc85b3a2012452f03a4fb7515329bf9f
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-03-10 19:47:57 -0400

    Cambiando query

--- src/database/query.sql
+++ src/database/query.sql
@@ -1,23 +1,12 @@
 CREATE DATABASE Prueba01;
 
-USE Prueba01;
-
 CREATE TABLE IF NOT EXISTS citas(
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_paciente VARCHAR(100) NOT NULL,
+    nombre varchar(100) not null,
     id_medico VARCHAR(100) NOT NULL,
     fecha DATE,
     hora TIME,
-    creado_en DATE,
-    editado_en DATE NULL
-);
-
-CREATE TABLE IF NOT EXISTS medicos (
-    id_medico INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    especialidad VARCHAR(100) NOT NULL,
-    telefono VARCHAR(100) NOT NULL,
-    direccion VARCHAR(255) NOT NULL,
     creado_en DATE,
     editado_en DATE NULL
 );
@@ -34,6 +23,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS pacientes (
     id_paciente INT auto_increment KEY,
+    email VARCHAR(100),
     nombre VARCHAR(100) NOT NULL,
     edad INT NOT NULL,
     sexo VARCHAR(100) NOT NULL,
@@ -44,17 +34,18 @@ CREATE TABLE IF NOT EXISTS pacientes (
     editado_en DATE NULL
 );
 
-CREATE TABLE IF NOT EXISTS horarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_medico VARCHAR(100) NOT NULL,
-    fecha DATE,
-    initial_date TIME,
-    final_date TIME,
-    cosnultorio VARCHAR(100),
+CREATE TABLE IF NOT EXISTS medicos (
+    id_medico INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    especialidad VARCHAR(100) NOT NULL,
+    telefono VARCHAR(100) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
     creado_en DATE,
     editado_en DATE NULL
 );
 
-INSERT INTO pacientes (nombre, edad, sexo, telefono, fecha_nacimiento, direccion, creado_en) VALUES ( 'Joselinho', '25', 'Masculino', '1234567890', '1996-01-01', 'Calle 123', '2021-01-01');
+INSERT INTO pacientes (email, nombre, edad, sexo, telefono, fecha_nacimiento, direccion, creado_en) VALUES ( 'usuario@usuario.com','Joselinho', '25', 'Masculino', '1234567890', '1996-01-01', 'Calle 123', '2021-01-01');
 
-Insert INTO medicos (id_medico, nombre, especialidad, telefono, direccion, creado_en) VALUES ('1', 'Dr. Juan Perez', 'Cirujano', '1234567890', 'Calle 123', '2021-01-01');
+Insert INTO medicos (nombre, especialidad, telefono, direccion, creado_en) VALUES ('Dr. Juan Perez', 'Cirujano', '1234567890', 'Calle 123', '2021-01-01');
+
+SELECT * FROM pacientes where email = "usuario@usuario.com";
