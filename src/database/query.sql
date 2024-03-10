d314fa2ada478c53e0df7fcf84aab8bdccaffbf1
Andres Pausin <candrespausin2001@gmail.com>
2024-03-10 14:33:06 -0400

    add: database tables query

--- src/database/query.sql
+++ src/database/query.sql
@@ -2,20 +2,34 @@ CREATE DATABASE Prueba01;
 
 USE Prueba01;
 
-CREATE TABLE citas(
+CREATE TABLE IF NOT EXISTS citas(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombrePaciente VARCHAR(100) NOT NULL,
-    nombreDoctor VARCHAR(100) NOT NULL,
+    id_paciente VARCHAR(100) NOT NULL,
+    id_medico VARCHAR(100) NOT NULL,
     fecha DATE,
     hora TIME,
     creado_en DATE,
-    editado_en DATE
+    editado_en DATE NULL
 );
 
-SELECT * FROM citas;
-
 CREATE TABLE IF NOT EXISTS users (
-    email varchar(100) NOT NULL PRIMARY KEY,
-    name varchar(50),
-    password varchar(255) NOT NULL
-  );
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    contrasena VARCHAR(100) NOT NULL,
+    id_user VARCHAR(100) NOT NULL,
+    tipo_cuenta VARCHAR(100) NOT NULL,
+    created_at DATE,
+    updated_at DATE NULL
+);
+
+CREATE TABLE IF NOT EXISTS pacientes (
+    id_paciente VARCHAR(100) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    edad INT NOT NULL,
+    sexo VARCHAR(100) NOT NULL,
+    telefono VARCHAR(100) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    creado_en DATE,
+    editado_en DATE NULL
+);
